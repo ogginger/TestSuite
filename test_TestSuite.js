e827c2e3508b9b1cb49c3c2a4b4ed3e1a14661a4
Josh <og.ginger@gmail.com>
2018-04-05 10:26:26 +0000

    Edited test_TestSuite.js: Added test: xGenerateTest_SimplePassingTest_ReturnsPassingTest.

--- test_TestSuite.js
+++ test_TestSuite.js
@@ -2,18 +2,21 @@
 
 define([
   "TestSuite_stable",
-  "Functions/log"
+  "Functions/log",
+  "tests/xGenerateTest_SimplePassingTest_ReturnsPassingTest"
 ], function(
   TestSuite_stable,
-  log
+  log,
+  xGenerateTest_SimplePassingTest_ReturnsPassingTest
 ) {
   return TestSuite_stable.extend({
     "initialize": function() {
       log("test_TestSuite initialized successfully!");
       var xTestSuite = this;
 
+	xTestSuite.add( xGenerateTest_SimplePassingTest_ReturnsPassingTest );
 
-      xTestSuite.test();
+	xTestSuite.bTestAll();
     }
   });
 });
