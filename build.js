c4349fa685f67db7ba0e112e23b3305a6dfa9c7d
Josh <og.ginger@gmail.com>
2018-10-02 09:49:15 +0000

    Updated testsuite.min..js: Optimized TestSuite.js after refactoring.

--- build.js
+++ build.js
@@ -8,10 +8,36 @@
 	"backbone": "lib/backbone",
 	"underscore": "lib/underscore",
 	"jquery": "lib/jquery",
-	"bIsObjectEqual": "Functions/bIsObjectEqual",
-	"log": "Functions/log"
+	"bIsObjectEqual": "lib/is_object_equal.min",
+	"log": "lib/log.min",
+	"test": "Functions/test",
+	"promise": "lib/promise.min",
+	"xGenerateTest": "lib/generate_test.min",
+	"xAddTest": "lib/add_test.min",
+	"bTestAll": "lib/test_all.min",
+	"xGenerateAsyncTest": "lib/generate_async_test.min",
+	"bTestAllAsync": "lib/test_all_async.min",
+	"async_every": "lib/async_every.min",
+	"AsyncIterator": "lib/async_iterator.min",
+	"AsyncValidate": "lib/AsyncValidate",
+	"validate": "lib/validate.min"
     },
-    exclude: [ "rsvp", "backbone", "underscore", "jquery" ],
+    exclude: [
+	
+	"AsyncValidate",
+	"AsyncIterator",
+	"async_every",
+	"bTestAllAsync",
+	"xGenerateAsyncTest",
+	"bTestAll",
+	"xAddTest",
+	"xGenerateTest",
+	"promise",
+	"rsvp", 
+	"backbone", 
+	"underscore", 
+	"jquery"
+    ],
     name: "TestSuite",
     out: "testsuite.min.js"
 })
